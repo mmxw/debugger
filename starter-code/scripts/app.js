c06adcd331432c41851cc0d31d4019fcc8c4237a
cara <xw33@outlook.com>
2019-12-22 23:25:19 +0000

    add random bomb generator function

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -2,19 +2,19 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
 function init() {
 
-  // VARIABLES
+  //! VARIABLES
 
   // *const excludedNumsArr = flat(_.range(0, width, [width * line, width * (line + 1) - 1], _.range(width * 11, width * 12)))
   const squares = [] //arrays of grids
   const clickedGrid = null //index of the grid that user clicked
   const width = 12
   let timerID = null
-  let bombCounter = null
+  const bombCounter = null
   let timeRemaining = 999
   let gameInPlay = false
-  let userIndex = null
+  const userIndex = null
 
-  // DOM VARIABLES
+  //! DOM VARIABLES
   const board = document.querySelector('.board')
   const playBtn = document.querySelector('.play')
   const firstClick = document.querySelector('.first-click') // first click triggers the revealing of nearby 5-20 grids 
@@ -25,14 +25,15 @@ function init() {
   
   
 
-  // FUNCTIONS
+  //! FUNCTIONS
+
+  //? SET UP GAME BOARD (MOSTLY DONE)
 
   // when user hits the `play` button, the game board is set (by clearing and generating the grid)
+  //TODO add an html element for the `play` button
   function play() {
     clearGrid()
     generateGrid()
-    
-
   }
 
   function generateGrid() {
@@ -42,11 +43,11 @@ function init() {
       squares.push(square)
       board.appendChild(square)
     })
-    // TODO timer()
+
     timer()
   }
 
-  console.log(squares) //* TESTING
+  // console.log(squares) //* TESTING
   //generateGrid() //* TESTING
 
 
@@ -60,7 +61,7 @@ function init() {
     }
   }
 
-  countDown()
+  // countDown() //* TESTING
 
   function finishGame() {
     // finish game; timer reset; 
@@ -71,28 +72,26 @@ function init() {
 
   function timer() {
     // count down by one second from 999
-    if (timerID === null) {
-      timerID = setInterval(countDown, 1000)
-    } else {
-      timerID = setInterval(countDown, 1000)
-    } 
-    console.log('creating', timerID, new Error().stack)
+    timerID = setInterval(countDown, 1000)
+    console.log('creating', timerID, new Error().stack) //* TESTING
   }
 
   function resetTimer() {
     clearInterval(timerID)
-    console.log('clearing', timerID)
+    console.log('clearing', timerID) //* TESTING
     timerDisplay.textContent = 0
   }
 
-  generateGrid()
+  generateGrid() //* TESTING
 
+  //*****************THE ABOVE IS ALL GOOD NOW; ADD STYLING LATER; ADD PLAY BUTTON LATER*/
+
+  //? USER STARTS GAME
 
   
   function startGame() {
     // TODO the first one clicked on is always (0), 
     // TODO then computer automatically click all surrounding grids (because it is impossible for them to have bombs, otherwise the current one won't be 0
-    
     
     randomizeBombs()
     
@@ -100,26 +99,29 @@ function init() {
   }
 
   function randomizeBombs() {
-    // any 10 grids on the board
-    // other than the first one clicked (let clicked = the index of the clicked grid) 
+    //TODO any 10 grids on the board
+    //TODO other than the first one clicked (let clicked = the index of the clicked grid) 
 
-    generateRandomNums(0, 143)
-    randomNumbers.forEach(number => board[number].classList.add('bomb'))
+    const randomBombIndex = generateRandomNums(0, 143)
+    randomBombIndex.forEach(index => squares[index].classList.add('bomb'))
   }
+
 
   function userClick(e) {
     // continue playing if game continues; else game over
     if (!gameInPlay) return 
 
     // if the one clicked on is a bomb then game over
-    // if the one clicked on is empty (0), computer automatically click all surrounding grids (because it is impossible for them to have bombs, otherwise the current one won't be 0
-    // computer stops autoclicking when there forms a wall with numbers of bombs displayed in the grids 
-
     if (e.target.classList.constains('bomb')) {
       finishGame()
+    } else {
+
     }
+    //TODO if the one clicked on is empty (0), computer automatically click all surrounding grids (because it is impossible for them to have bombs, otherwise the current one won't be 0
+    //TODO computer stops autoclicking when there forms a wall with numbers of bombs displayed in the grids 
 
     
+
   }
 
   function generateRandomNums(min, max) {
@@ -130,25 +132,24 @@ function init() {
       // eslint-disable-next-line func-call-spacing
       const randomNum = Math.floor(Math.random() * (max - min + 1)) + min
       
-      (randomNumbers.has(excludedNumArr) || randomNumbers.has(firstClick)) ? generateRandomNums(min, max) : randomNum
+      randomNumbers.has(excludedNumArr) || randomNumbers.has(squares.indexOf(firstClick)) ? generateRandomNums(min, max) : randomNum
       
 	    randomNumbers.add(randomNum)
     }
-
+    
     return randomNumbers
-
   }
 
   
 
   function countBombs() {	
-    // for each grid, count the neighboring 8 squares, if they have '.bomb' class, if so, counter ++
+    //TODO for each grid, count the neighboring 8 squares, if they have '.bomb' class, if so, counter ++
 
 
   }
 
   function flag() {
-    // right click to put a flag on the grid (if user decides that there is definitely a bomb, then they flag it)
+    //TODO right click to put a flag on the grid (if user decides that there is definitely a bomb, then they flag it)
 
   }
 
@@ -160,27 +161,28 @@ function init() {
   }
 
   function resetBoard() {
-    // clear all bombs
-    // clear all flags
-    // return to starting position
+    //TODO clear all bombs
+    //TODO clear all flags
+    //TODO return to starting position
 
   }
 
 
 
 
-  // DOM EVENTS
+  //TODO DOM EVENTS
 
-  // start game
-  //playBtn.addEventListener('click', play)
+  // SET UP GAME BOARD
+  //playBtn.addEventListener('click', play) //setting up board, timer and flags; timer starts to count down; 
 
+  // USER STARTS TO PLAY GAME
   // firstClick.addEventListener('click', startGame)
 
   
-    // squares.forEach(square => {
-    //   square.addEventListener('click', () => {
-    //       console.log(squares.indexOf(square))
-    //   })
+  // squares.forEach(square => {
+  //   square.addEventListener('click', () => {
+  //       console.log(squares.indexOf(square))
+  //   })
   
 
 
